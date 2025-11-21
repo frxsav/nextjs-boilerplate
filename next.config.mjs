13d39dc0fe0497af8dcd220bd2081d5a743c27ed
frxsav <keccofor@gmail.com>
2025-11-21 16:32:51 +0100

    added CSP config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `
+      default-src 'self';
+      img-src 'self' data: https:;
+      script-src 'self' 'unsafe-inline' https:;
+      style-src 'self' 'unsafe-inline';
+      font-src 'self' https: data:;
+      connect-src 'self' https:;
+    `
+      .replace(/\s{2,}/g, ' ')
+      .trim(),
+  },
+];
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
